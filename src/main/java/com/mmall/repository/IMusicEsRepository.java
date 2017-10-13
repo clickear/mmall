9793f1b408a45409eaf120a09236c07dd78b5e7d
clickear <clickear@163.com>
2017-10-13 23:32:27 +0800

    add es search and change jackson

--- src/main/java/com/mmall/repository/IMusicEsRepository.java
+++ src/main/java/com/mmall/repository/IMusicEsRepository.java
@@ -1,6 +1,7 @@
 package com.mmall.repository;
 
 import com.mmall.pojo.MusicSong;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
@@ -9,5 +10,9 @@ import java.util.List;
  * Created by Administrator on 2017/10/11.
  */
 public interface  IMusicEsRepository extends ElasticsearchRepository<MusicSong, Long> {
+
+    List<MusicSong> findBySongNameLike(String songName);
+
+    List<MusicSong> findByArtistName(String artistName);
 
 }
